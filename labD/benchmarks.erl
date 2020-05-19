fea3a14753890a0486caa77a942f3539452bc608
AdamOlsson <Olsson.95_@hotmail.com>
2020-05-19 12:15:44 +0200

    simplfied names

--- labD/benchmarks.erl
+++ labD/benchmarks.erl
@@ -3,9 +3,9 @@
 
 -define(EXECUTIONS,1).
 
-map_reduce_par()            -> benchmark(page_rank_par).
-map_reduce_dist()           -> benchmark(page_rank_dist).
-map_reduce_load_balance()   -> benchmark(page_rank_load_balance).
+par()            -> benchmark(page_rank_par).
+dist()           -> benchmark(page_rank_dist).
+load_balance()   -> benchmark(page_rank_load_balance).
 
 benchmark(F) -> lists:sum(lists:flatten(lists:map(fun(_) -> 
                                             [T || {T, _} <- [timer:tc(page_rank, F, [])]]
